f872183dba523213e6b7c344b6320d7941a2eeaf
Greg Ziegan <greg.ziegan@gmail.com>
2024-04-23 15:20:15 -0500

    forgot to save

--- hosts/fred/default.nix
+++ hosts/fred/default.nix
@@ -42,21 +42,6 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  users.users.gziegan = {
-    isNormalUser = true;
-    description = "Greg Ziegan";
-    extraGroups = [ "networkmanager" "wheel" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhQYc5LT5giAndbsAoMC9/bVUhDWcD4pm4am8BMEK55 greg.ziegan@gmail.com"
-    ];
-    packages = with pkgs; [
-      firefox
-      chromium
-      vscode
-    ];
-  };
-
-  users.users.root.openssh.authorizedKeys.keys = config.users.users.gziegan.openssh.authorizedKeys.keys;
 
   nixpkgs.config.allowUnfree = true;
 
